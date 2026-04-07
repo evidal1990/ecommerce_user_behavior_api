16e0579067b342cc7c9a1087fd865845f745ee3b
Edith Vidal <ecsr.vidal@gmail.com>
2026-04-07 13:29:27 -0300

    Adicionando config e conexão com o supabase

---
+++ src/core/database.py
@@ -0,0 +1,12 @@
+import psycopg2
+from src.core.config import settings
+
+
+def get_connection():
+    return psycopg2.connect(
+        host=settings.DB_HOST,
+        database=settings.DB_NAME,
+        user=settings.DB_USER,
+        password=settings.DB_PASSWORD,
+        port=settings.DB_PORT,
+    )
