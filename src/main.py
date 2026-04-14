281436bdc56172a42dafe298e74b7708959cc17e
Edith Vidal <ecsr.vidal@gmail.com>
2026-04-14 11:45:06 -0300

    Adicionando rotas / e /health na main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 from src.api.router import api_router
 
-app = FastAPI(title="Supabase API")
+app = FastAPI(title="Ecommerce User Behavior API")
+
+
+@app.get("/")
+def root():
+    return {
+        "status": "ok",
+        "service": "Ecommerce User Behavior API",
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy",
+    }
+
 
 app.include_router(api_router)
